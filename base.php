5e7ae5b055d48622b6d2357e8e4ea0255ecaca4b
Thomas Wong <tomaswolfgang@gmail.com>
2015-11-18 16:12:20 -0500

    jquery/and base html for webpage
    Just html skeleton
    nothing crazy yet

---
+++ base.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+	<html>
+		<head>
+			
+
+		</head>
+		<body>
+			<div id="welcomecontainer" class="pagecontainer">
+				<div id="welcome" class="title"></div>
+				<div id="loginbox">
+					<div id="username"></div>
+					<div id="password"></div>
+				</div>
+				<div id="register"></div>
+				<div class="downscrollbutton"></div>
+			</div>
+
+			<div id="purposecontainer" class="pagecontainer">
+				<div class="upscrollbutton"></div>
+				<div id="purpose" class="title"></div>
+				<div id="purposeParagraph"></div>
+				<div class="downscrollbutton"></div>
+			</div>
+			
+			<!-- use json html iteration here -->
+			<div id="technologycontainer" class="pagecontainer">
+				<div id="techbubblecontainer">
+					<div id="frontendtechtitle" class="subtitle"></div>
+					<div id="frontendbubbles"></div>
+					<div id="backendtechtitle" class="subtitle"></div>
+					<div id="backendbubbles"></div>
+				</div>
+			</div>
+			
+
+			<!-- use bootstrap for this list -->
+			<div id="learncontainer">
+				<div id="learnpositive">
+					<li id="poslist">   
+						<ul></ul>
+						<ul></ul>
+						<ul></ul>
+					</li>
+				</div>
+				<div id="learnnegative">
+					<li id="neglist">
+						<ul></ul>
+						<ul></ul>
+						<ul></ul>
+					</li>
+				</div>
+			</div>
+
+			
+		</body>
+	</html>
+
+	<script type="text/javascript">
+	
+
+	</script>
+
+	<script type="text/javascript" src="js/jquery.min.js?v=1.11.3" async onload="jqloaded=true;onloadCallback()">//async loading of scripts</script>
